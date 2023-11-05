6657ec515cf78d0b020345d221cc7a02b6f4f763
lanath <valentin.vivier@bhc-it.com>
2023-11-05 20:22:40 +0100

    mushu

--- home/mushu-laptop/hypr.nix
+++ home/mushu-laptop/hypr.nix
@@ -1,5 +1,5 @@
 ''
-monitor=eDP-1,1920x1080@144,0x0,1
+monitor=eDP-1,1920x1080@60,0x0,1
 env = XCURSOR_SIZE,24
 exec-once = swww init && swww img ${./nord-city.jpeg}
 exec-once = wl-paste --watch cliphist store
