8c9b63d9a04a03bdff3bc158320435224a8512b6
lanath <valentin.vivier@viviersoft.com>
2024-01-26 11:02:28 +0100

    hardware

--- config/lanath-laptop/hardware-configuration.nix
+++ config/lanath-laptop/hardware-configuration.nix
@@ -1,37 +1,30 @@
-{ config, lib, pkgs, modulesPath, ... }:
 {
   imports =
     [
       (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.supportedFilesystems = [ "nfs" ];
-  boot.initrd.kernelModules = [ "kvm-intel" "nfs" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
   fileSystems."/" =
     {
-      device = "/dev/disk/by-uuid/a8b45e48-e5aa-4231-9725-697c869c4356";
+      device = "/dev/disk/by-uuid/7d1cb5f1-0fca-46e3-a2eb-63404ebfde03";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
     {
-      device = "/dev/disk/by-uuid/3B98-19EA";
+      device = "/dev/disk/by-uuid/BCAA-8876";
       fsType = "vfat";
     };
 
+  swapDevices = [ ];
 
-  fileSystems."/mnt/lanath" = {
-    device = "nfs.master.monkey:/lanath";
-    fsType = "nfs";
-    options = [ "rw" "user" "noauto" "x-systemd.automount" "x-systemd.idle-timeout=600" ];
-  };
+  networking.useDHCP = lib.mkDefault true;
 
-  swapDevices =
-    [{ device = "/dev/disk/by-uuid/210c39d2-7ca4-4437-894f-7f674aca4106"; }];
-
-  networking.useDHCP = lib.mkForce true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
