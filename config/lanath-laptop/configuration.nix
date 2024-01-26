dea83a7841689d6c110ff325fe7b7c86f4432f9e
lanath <valentin.vivier@viviersoft.com>
2024-01-26 09:49:36 +0100

    hardware

--- config/lanath-laptop/configuration.nix
+++ config/lanath-laptop/configuration.nix
@@ -84,13 +84,6 @@ in
     etcher
   ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-  hardware.opengl.driSupport32Bit = true; # Enables support for 32bit libs that steam uses
-
   nixpkgs.config.permittedInsecurePackages = [
     "electron-19.1.9"
     "electron-12.2.3"
--- config/lanath-laptop/hardware-configuration.nix
+++ config/lanath-laptop/hardware-configuration.nix
@@ -17,20 +17,17 @@
   boot.initrd.supportedFilesystems = [ "nfs" ];
   boot.initrd.kernelModules = [ "kvm-intel" "nfs" ];
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/a8b45e48-e5aa-4231-9725-697c869c4356";
+    {
+      device = "/dev/disk/by-uuid/a8b45e48-e5aa-4231-9725-697c869c4356";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3B98-19EA";
+    {
+      device = "/dev/disk/by-uuid/3B98-19EA";
       fsType = "vfat";
     };
 
-  fileSystems."/home/username" = {
-    device = "none";
-    fsType = "tmpfs";
-    options = [ "size=4G" "mode=777" ];
-  };
 
   fileSystems."/mnt/lanath" = {
     device = "nfs.master.monkey:/lanath";
@@ -39,12 +36,11 @@
   };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/210c39d2-7ca4-4437-894f-7f674aca4106"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/210c39d2-7ca4-4437-894f-7f674aca4106"; }];
 
   networking.useDHCP = lib.mkForce true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
