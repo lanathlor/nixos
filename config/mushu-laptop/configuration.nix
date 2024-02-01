c457bc70a86a6e71c44c362149fdc59cf3fcabf2
lanath <valentin@viviersoft.com>
2024-02-01 14:26:21 +0100

    fr

--- config/mushu-laptop/configuration.nix
+++ config/mushu-laptop/configuration.nix
@@ -133,7 +133,16 @@ in
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.gnome = {
+    enable = true;
+    extraGSettingsOverrides = ''
+      [org.gnome.desktop.input-sources]
+      sources='[('xkb', 'fr')]'
+    '';
+    extraGSettingsOverridePackages = [
+      pkgs.gsettings-desktop-schemas
+    ];
+  };
   services.xserver.displayManager.sddm.enable = lib.mkForce false;
   xdg.portal.enable = lib.mkForce false;
 
