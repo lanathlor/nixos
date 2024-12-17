dbf17c8299bd3a493dbcdf23b696e10ad5ca9c02
lanath <valentin@viviersoft.com>
2024-12-17 10:18:40 +0100

    fix: typos and paths

--- modules/system/user/lanath.nix
+++ modules/system/user/lanath.nix
@@ -1,6 +1,6 @@
 { ... }:
 let
-  lanath = import ./lanath.nix {
+  lanath = import ./default.nix {
     username = "lanath";
     initialHashedPassword = "$y$j9T$TFdhvKQ4clM.JxX1ScPkq1$tOxZv2DOIBWF/uhoyfCbzIkCYZuwa9BfEPNI4wmzqN3";
   };
